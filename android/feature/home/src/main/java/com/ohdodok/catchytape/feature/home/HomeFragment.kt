f0dde5215b45367281c747bd6dbcf0d2cfd501bf
algosketch <algosketch@gmail.com>
2023-11-10 21:37:16 +0900

    feat : navigation에 HomeFragment 연결

---
+++ android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeFragment.kt
@@ -0,0 +1,8 @@
+package com.ohdodok.catchytape.feature.home
+
+import com.ohdodok.catchytape.core.ui.BaseFragment
+import com.ohdodok.catchytape.feature.home.databinding.FragmentHomeBinding
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+}
