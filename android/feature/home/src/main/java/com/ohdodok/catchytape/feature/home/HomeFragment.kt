367a7c94989a99b84c64a9c147bac6c0d8bd96c8
algosketch <algosketch@gmail.com>
2023-11-10 21:53:09 +0900

    feat : HomeViewModel 생성

--- android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeFragment.kt
+++ android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeFragment.kt
@@ -1,8 +1,17 @@
 package com.ohdodok.catchytape.feature.home
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
 import com.ohdodok.catchytape.core.ui.BaseFragment
 import com.ohdodok.catchytape.feature.home.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+    private val viewModel: HomeViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+    }
 
 }
---
+++ android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.ohdodok.catchytape.feature.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+}
