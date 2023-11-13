19c7c377afb77d48fc5a4c43d21c8a4b9f6daf32
2taezeat <xorudfl772@naver.com>
2023-11-13 20:22:02 +0900

    feat : MusicHorizontalAdapter 구현 및 연동

---
+++ android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/model/Music.kt
@@ -0,0 +1,8 @@
+package com.ohdodok.catchytape.core.domain.model
+
+data class Music(
+    val id: String,
+    val title: String,
+    val artist: String,
+    val imageUrl: String
+)
--- android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeFragment.kt
+++ android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeFragment.kt
@@ -12,6 +12,6 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
+        binding.rvRecentlyAddedSong.adapter = MusicHorizontalAdapter()
     }
-
 }
