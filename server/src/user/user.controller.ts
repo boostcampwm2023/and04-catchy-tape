bdf622d3d01804d5de9550d35025858d4971352b
hyungun <khw3754@gmail.com>
2023-11-13 23:41:51 +0900

    feat : 닉네임 중복 검사 API 구현
    * 닉네임 중복 검사 API 구현
    * 중복 - 20010
    * 중복 아님 - 40010
    
    Co-authored-by: Cutiepazzipozzi <sk000801@naver.com>

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,13 +1,44 @@
-import { Body, Post, UsePipes, ValidationPipe } from "@nestjs/common";
-import { UserService } from "./user.service";
-import { UserCreateDto } from "src/dto/userNickname.dto";
+import {
+  Body,
+  Get,
+  HttpCode,
+  HttpException,
+  Param,
+  Post,
+  Res,
+  Response,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserCreateDto } from 'src/dto/userCreate.dto';
+import { HTTP_STATUS_CODE } from 'src/httpStatusCode.enum';
 
 export class UserController {
-    constructor(private userService: UserService){}
+  constructor(private userService: UserService) {}
 
-    @Post('signup')
-    @UsePipes(ValidationPipe)
-    signup(@Body() userCreateDto: UserCreateDto) {
-        this.userService.createUser(userCreateDto);
+  @Post('signup')
+  @UsePipes(ValidationPipe)
+  @HttpCode(20000)
+  signup(@Body() userCreateDto: UserCreateDto): string {
+    try {
+      const userId: string = this.userService.createUser(userCreateDto);
+      return userId;
+    } catch {
+      throw new HttpException('SERVER ERROR', HTTP_STATUS_CODE.SERVER_ERROR);
     }
+  }
+
+  @Get('duplicate/:name')
+  @HttpCode(HTTP_STATUS_CODE.NOT_DUPLICATED_NICKNAME)
+  async checkDuplicateNickname(@Param('name') name: string): Promise<string> {
+    if (await this.userService.isDuplicatedUserEmail(name)) {
+      throw new HttpException(
+        'DUPLICATED_NICKNAME',
+        HTTP_STATUS_CODE.DUPLICATED_NICKNAME,
+      );
+    }
+
+    return name;
+  }
 }
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,24 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { UserCreateDto } from 'src/dto/userNickname.dto';
+import { HttpException, Injectable } from '@nestjs/common';
+import { HTTP_STATUS_CODE } from 'src/httpStatusCode.enum';
+import { UserCreateDto } from 'src/dto/userCreate.dto';
 import { User } from 'src/entity/user.entity';
 import { Repository } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class UserService {
-    //TODO: custom repository로 변경하기
-    constructor(private userRepository: Repository<User>) {}
+  //TODO: custom repository로 변경하기
+  constructor(private userRepository: Repository<User>) {}
 
-    createUser(userCreateDto: UserCreateDto) {
-        const {nickname, email} = userCreateDto;
-        const newUser: User = {
-            userId: uuid(),
-            nickname,
-            photo: null,
-            user_email: email,
-            created_at: new Date()
-        }
-        const user = this.userRepository.create(newUser);
-        this.userRepository.save(user);
+  createUser(userCreateDto: UserCreateDto): string {
+    const { nickname, email } = userCreateDto;
+    const newUser: User = {
+      userId: uuid(),
+      nickname,
+      photo: null,
+      user_email: email,
+      created_at: new Date(),
+    };
+    const user = this.userRepository.create(newUser);
+    this.userRepository.save(user);
+    return newUser.userId;
+  }
+
+  async isDuplicatedUserEmail(userNickname: string): Promise<boolean> {
+    try {
+      const user = await this.userRepository.findOneBy({
+        nickname: userNickname,
+      });
+
+      if (user) {
+        return true;
+      }
+
+      return false;
+    } catch {
+      throw new HttpException('SERVER ERROR', HTTP_STATUS_CODE.SERVER_ERROR);
     }
+  }
 }
