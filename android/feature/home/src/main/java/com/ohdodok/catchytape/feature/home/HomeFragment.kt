425897650f2386629c8c999ae8f7b7541902f0bc
youlalala <reyoula@gmail.com>
2023-11-14 11:28:38 +0900

    chore : hilt 설정

--- android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeFragment.kt
+++ android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeFragment.kt
@@ -5,7 +5,9 @@ import android.view.View
 import androidx.fragment.app.viewModels
 import com.ohdodok.catchytape.core.ui.BaseFragment
 import com.ohdodok.catchytape.feature.home.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val viewModel: HomeViewModel by viewModels()
 
