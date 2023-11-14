3c37cf2681beaa68e1baf6e66ef2a5d7cb0e636b
hyungun <khw3754@gmail.com>
2023-11-14 16:45:32 +0900

    fix : user 모듈 오류 수정
    * 회원가입, 닉네임 중복검사 API 오류 발견
    * typeorm 설정 오류 수정
    * repository 가 잘 등록이 안되어 있는 부분 수정
    * src/typeorm.config.ts 는 다음 커밋에서 추가해야함

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService],
 })
 export class UserModule {}
