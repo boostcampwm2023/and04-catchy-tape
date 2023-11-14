d78464953ab4aff581b5cf7185eb7ba687cefd49
2taezeat <xorudfl772@naver.com>
2023-11-14 22:03:11 +0900

    feat : timer 적용

--- android/app/src/main/java/com/ohdodok/catchytape/CtApplication.kt
+++ android/app/src/main/java/com/ohdodok/catchytape/CtApplication.kt
@@ -2,6 +2,22 @@ package com.ohdodok.catchytape
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class CtApplication : Application()
+class CtApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(object : Timber.DebugTree() {
+                override fun createStackElementTag(element: StackTraceElement): String {
+                    return String.format(
+                        applicationContext.getString(R.string.timber_log_format),
+                        super.createStackElementTag(element),
+                        element.lineNumber,
+                    )
+                }
+            })
+        }
+    }
+}
--- android/app/src/main/java/com/ohdodok/catchytape/MainActivity.kt
+++ android/app/src/main/java/com/ohdodok/catchytape/MainActivity.kt
@@ -3,11 +3,13 @@ package com.ohdodok.catchytape
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.d("hello")
     }
 }
