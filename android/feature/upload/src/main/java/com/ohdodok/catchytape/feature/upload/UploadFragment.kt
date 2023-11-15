c24197d8bdeb91b33363a8ec59a5fbd1c8ee1135
algosketch <algosketch@gmail.com>
2023-11-15 13:55:09 +0900

    chore : uploadFragment 생성

---
+++ android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadFragment.kt
@@ -0,0 +1,17 @@
+package com.ohdodok.catchytape.feature.upload
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import com.ohdodok.catchytape.catchytape.upload.R
+import com.ohdodok.catchytape.catchytape.upload.databinding.FragmentUploadBinding
+import com.ohdodok.catchytape.core.ui.BaseFragment
+
+class UploadFragment : BaseFragment<FragmentUploadBinding>(R.layout.fragment_upload) {
+    private val viewModel: UploadViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+    }
+}
