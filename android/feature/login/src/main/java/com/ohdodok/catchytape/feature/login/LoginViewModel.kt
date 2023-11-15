85323ea9fca8472d659c779251a18312f9efea15
2taezeat <xorudfl772@naver.com>
2023-11-15 22:12:30 +0900

    feat : LoginActivity 추가 및 반영

--- android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/LoginViewModel.kt
+++ android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/LoginViewModel.kt
@@ -1,10 +1,7 @@
 package com.ohdodok.catchytape.feature.login
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
 
-@HiltViewModel
 class LoginViewModel : ViewModel() {
-
 
 }
