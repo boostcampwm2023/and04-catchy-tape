a389c40193f64a1709e1b8e1a7a01150ceb96143
hyungun <khw3754@gmail.com>
2023-11-16 04:46:05 +0900

    feat : jwt.strategy.ts 파일 추가
    * 토큰 유효성 검사를 해주는 guard 를 사용하기 위해 jwt.strategy.ts 구현
    * JwtModule import 를 user module 로 옮겨줌
    * ConfigService 를 사용하여 env 파일에서 JWT_SECRET_KEY 를 가져오도록 구현

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -3,10 +3,26 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule],
 })
 export class UserModule {}
