291c1daf70885dd66c39b5ca603e720383c59b70
2taezeat <xorudfl772@naver.com>
2023-11-15 21:36:19 +0900

    feat : fragment_login UI 개선

--- android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/LoginViewModel.kt
+++ android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/LoginViewModel.kt
@@ -1,7 +1,9 @@
 package com.ohdodok.catchytape.feature.login
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@HiltViewModel
 class LoginViewModel : ViewModel() {
 
 
