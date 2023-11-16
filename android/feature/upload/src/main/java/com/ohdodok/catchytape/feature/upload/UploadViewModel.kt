16ea13f3129c82b4d3a425dd0e5eb8e3e55e76c9
2taezeat <xorudfl772@naver.com>
2023-11-16 22:18:10 +0900

    feat : isUploadEnable with flow

--- android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadViewModel.kt
+++ android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadViewModel.kt
@@ -1,16 +1,40 @@
 package com.ohdodok.catchytape.feature.upload
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class UploadViewModel : ViewModel() {
 
-    val uploadMusicTitle = MutableStateFlow<String>("")
+    val uploadedMusicTitle = MutableStateFlow<String>("")
 
-    private val _uploadMusicGenrePosition = MutableStateFlow(0)
-    val uploadMusicGenrePosition = _uploadMusicGenrePosition.asStateFlow()
+    private val _uploadedMusicGenrePosition = MutableStateFlow(0)
+    val uploadedMusicGenrePosition = _uploadedMusicGenrePosition.asStateFlow()
+
+    private val _isUploadEnable = MutableStateFlow(false)
+    val isUploadEnable = _isUploadEnable.asStateFlow()
+
+    init {
+        observeMusicTitle()
+        observeGenrePosition()
+    }
+
+    val onChangePosition: (Int) -> Unit = { position: Int -> _uploadedMusicGenrePosition.value = position }
 
 
-    val onChangePosition: (Int) -> Unit = { position: Int -> _uploadMusicGenrePosition.value = position }
+    private fun observeMusicTitle() {
+        uploadedMusicTitle.onEach {
+            _isUploadEnable.value = uploadedMusicGenrePosition.value != 0 && it.isNotEmpty()
+        }.launchIn(viewModelScope)
+    }
+
+
+    private fun observeGenrePosition() {
+        uploadedMusicGenrePosition.onEach {
+            _isUploadEnable.value = it != 0 && uploadedMusicTitle.value.isNotEmpty()
+        }.launchIn(viewModelScope)
+    }
 }
