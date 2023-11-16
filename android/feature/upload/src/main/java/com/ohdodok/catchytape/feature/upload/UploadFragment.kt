45f9c585d17be7f0dc68caaac3d0c0b37a07026a
algosketch <algosketch@gmail.com>
2023-11-16 18:59:49 +0900

    feat : 이미지 파일 선택

--- android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadFragment.kt
+++ android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadFragment.kt
@@ -2,16 +2,37 @@ package com.ohdodok.catchytape.feature.upload
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
+import androidx.core.net.toFile
 import androidx.fragment.app.viewModels
 import com.ohdodok.catchytape.catchytape.upload.R
 import com.ohdodok.catchytape.catchytape.upload.databinding.FragmentUploadBinding
 import com.ohdodok.catchytape.core.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class UploadFragment : BaseFragment<FragmentUploadBinding>(R.layout.fragment_upload) {
     private val viewModel: UploadViewModel by viewModels()
+
+    private val pickMedia by lazy {
+        registerForActivityResult(PickVisualMedia()) { uri ->
+            if (uri == null) return@registerForActivityResult
+
+            viewModel.uploadImage(uri.toFile())
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
+
+        setupSelectThumbnailImage()
+    }
+
+    private fun setupSelectThumbnailImage() {
+        binding.cvUploadThumbnail.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+        }
     }
 }
--- android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadViewModel.kt
+++ android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadViewModel.kt
@@ -1,7 +1,19 @@
 package com.ohdodok.catchytape.feature.upload
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import java.io.File
+import javax.inject.Inject
 
-class UploadViewModel : ViewModel() {
+@HiltViewModel
+class UploadViewModel @Inject constructor(
 
+) : ViewModel() {
+
+    private var uploadedImage: String? = null
+
+    fun uploadImage(imageFile: File) {
+        // todo : image 파일을 업로드 한다.
+        // todo : 반환 값을 uploadedImage에 저장한다.
+    }
 }
