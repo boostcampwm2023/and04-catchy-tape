27c8381065442dfb1211c1903a8fd6ff338dd3c0
hyungun <khw3754@gmail.com>
2023-11-17 00:16:33 +0900

    feat : User nickname 글자수 제한 추가
    * 닉네임 2글자 이상 10글자 이하 규칙 추가
    * class-validator 활용

--- server/src/dto/userCreate.dto.ts
+++ server/src/dto/userCreate.dto.ts
@@ -1,8 +1,10 @@
-import { IsNotEmpty, IsString, Matches } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class UserCreateDto {
     @IsNotEmpty()
     @IsString()
+    @MinLength(2, {message: '닉네임이 2글자 미만입니다.'})
+    @MaxLength(10, {message: '닉네임이 10글자 초과입니다.'})
     nickname: string;
 
     @IsNotEmpty()
