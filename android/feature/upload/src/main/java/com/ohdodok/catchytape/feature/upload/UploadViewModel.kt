dbe7cd9889e1283fc02491666f035e5b8373df3c
algosketch <algosketch@gmail.com>
2023-11-16 23:00:59 +0900

    refactor : 업로드 가능 여부 판단 로직 간소화

--- android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadViewModel.kt
+++ android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadViewModel.kt
@@ -6,9 +6,13 @@ import java.io.File
 import javax.inject.Inject
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
 class UploadViewModel @Inject constructor(
@@ -17,33 +21,18 @@ class UploadViewModel @Inject constructor(
 
     private var uploadedImage: String? = null
 
-    val uploadedMusicTitle = MutableStateFlow<String>("")
+    val uploadedMusicTitle = MutableStateFlow("")
 
     private val _uploadedMusicGenrePosition = MutableStateFlow(0)
     val uploadedMusicGenrePosition = _uploadedMusicGenrePosition.asStateFlow()
 
-    private val _isUploadEnable = MutableStateFlow(false)
-    val isUploadEnable = _isUploadEnable.asStateFlow()
-    
+    val isUploadEnable: StateFlow<Boolean> =
+        combine(uploadedMusicTitle, uploadedMusicGenrePosition) { title, genrePosition ->
+            title.isNotBlank() && genrePosition != 0
+        }.stateIn(viewModelScope, SharingStarted.Eagerly, false)
+
     val onChangePosition: (Int) -> Unit = { position: Int -> _uploadedMusicGenrePosition.value = position }
 
-    init {
-        observeMusicTitle()
-        observeGenrePosition()
-    }
-
-    private fun observeMusicTitle() {
-        uploadedMusicTitle.onEach {
-            _isUploadEnable.value = uploadedMusicGenrePosition.value != 0 && it.isNotEmpty()
-        }.launchIn(viewModelScope)
-    }
-
-    private fun observeGenrePosition() {
-        uploadedMusicGenrePosition.onEach {
-            _isUploadEnable.value = it != 0 && uploadedMusicTitle.value.isNotEmpty()
-        }.launchIn(viewModelScope)
-    }
-    
     fun uploadImage(imageFile: File) {
         // todo : image 파일을 업로드 한다.
         // todo : 반환 값을 uploadedImage에 저장한다.
