68345f1638d448be7140e2d4eb5afa3206631181
Seokyung Lim <sk000801@naver.com>
2023-11-16 20:19:16 +0900

    feat : 음악 장르 enum 추가

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,3 +2,14 @@ export const fileSize: Record<string, number> = {
   MUSIC_FILE_LIMIT_SIZE: 1024 * 1024 * 50,
   IMAGE_FILE_LIMIT_SIZE: 1024 * 1024 * 5,
 };
+
+export enum Genres {
+  'hip-hop' = 'hip-hop',
+  'acoustic' = 'acoustic',
+  'ballade' = 'ballade',
+  'r&b' = 'r&b',
+  'jazz' = 'jazz',
+  'rock' = 'rock',
+  'dance' = 'dance',
+  'etc' = 'etc',
+}
