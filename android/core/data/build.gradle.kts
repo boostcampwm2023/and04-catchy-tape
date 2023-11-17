b053113b670aee8034d9ca3eb93d0a569d509d6b
youlalala <reyoula@gmail.com>
2023-11-17 20:05:49 +0900

    chore : Preference Datastore 추가

--- android/core/data/build.gradle.kts
+++ android/core/data/build.gradle.kts
@@ -40,5 +40,6 @@ dependencies {
     implementation(libs.okhttp.logging.interceptor)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.kotlinx.serialization.converter)
+    implementation(libs.datastore.preferences)
 
 }
