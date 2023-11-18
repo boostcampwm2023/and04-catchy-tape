dc90199fb60a1713ed160c5dbe22993570d7018b
youlalala <reyoula@gmail.com>
2023-11-18 13:17:22 +0900

    refactor : usecase flow 중첩 리팩토링

--- android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/usecase/LoginUseCase.kt
+++ android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/usecase/LoginUseCase.kt
@@ -2,22 +2,13 @@ package com.ohdodok.catchytape.core.domain.usecase
 
 import com.ohdodok.catchytape.core.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class LoginUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
 
-    operator fun invoke(googleToken: String): Flow<Unit> = flow {
-        authRepository.loginWithGoogle(googleToken)
-            .catch {
-                throw Exception("존재하지 않는 유저입니다.")
-            }.collect {
-                // 존재하는 유저
-                authRepository.saveToken(it)
-            }
-    }
+    operator fun invoke(googleToken: String): Flow<Unit> =
+        authRepository.loginWithGoogle(googleToken).map { authRepository.saveToken(it) }
 }
--- android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/usecase/SignUpUseCase.kt
+++ android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/usecase/SignUpUseCase.kt
@@ -2,18 +2,14 @@ package com.ohdodok.catchytape.core.domain.usecase
 
 import com.ohdodok.catchytape.core.domain.repository.AuthRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class SignUpUseCase @Inject constructor(
     private val authRepository: AuthRepository
 ) {
 
-    operator fun invoke(googleToken: String, nickname: String): Flow<Unit> = flow {
-        authRepository.signUpWithGoogle(googleToken, nickname).collect {
-                authRepository.saveToken(it)
-                emit(Unit)
-            }
-    }
+    operator fun invoke(googleToken: String, nickname: String): Flow<Unit> =
+        authRepository.signUpWithGoogle(googleToken, nickname).map { authRepository.saveToken(it) }
+
 }
