f7abaeb0d6ba91c5022c69212506ba90ba10518d
2taezeat <xorudfl772@naver.com>
2023-11-19 03:15:07 +0900

    feat : checkAutoLoginIsFinish

--- android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/LoginActivity.kt
+++ android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.ohdodok.catchytape.feature.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
+    private val viewModel: LoginViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
