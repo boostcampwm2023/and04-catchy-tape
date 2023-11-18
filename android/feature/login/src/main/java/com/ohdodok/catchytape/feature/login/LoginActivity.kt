17e097e9633dd4cb396f0aa67321835e26740e45
2taezeat <xorudfl772@naver.com>
2023-11-19 03:33:58 +0900

    feat : isSplashFinishReady 변수로 추출

--- android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/LoginActivity.kt
+++ android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/LoginActivity.kt
@@ -16,8 +16,33 @@ import kotlinx.coroutines.launch
 class LoginActivity : AppCompatActivity() {
     private val viewModel: LoginViewModel by viewModels()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        var isSplashFinishReady = false
+        viewModel.autoLoginWithIdToken()
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    lifecycleScope.launch {
+                        repeatOnLifecycle(Lifecycle.State.STARTED) {
+                            viewModel.isAutoLoginFinish.collect { isSplashFinishReady = it }
+                        }
+                    }
+
+                    if (isSplashFinishReady) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+            }
+        )
     }
 }
+
+
