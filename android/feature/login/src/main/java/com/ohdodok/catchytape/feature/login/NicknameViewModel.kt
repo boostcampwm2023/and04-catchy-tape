1c79bde20b80e1a26acf49735438feb2262d2bc7
HamBP <sjy_9484@naver.com>
2023-11-20 18:09:01 +0900

    feat : 닉네임 유효성 검사 결과를 저장하는 변수 생성

--- android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/NicknameViewModel.kt
+++ android/feature/login/src/main/java/com/ohdodok/catchytape/feature/login/NicknameViewModel.kt
@@ -2,18 +2,25 @@ package com.ohdodok.catchytape.feature.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ohdodok.catchytape.core.domain.signup.NicknameValidationResult
+import com.ohdodok.catchytape.core.domain.signup.NicknameValidationUseCase
 import com.ohdodok.catchytape.core.domain.usecase.SignUpUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 @HiltViewModel
 class NicknameViewModel @Inject constructor(
-    private val signUpUseCase: SignUpUseCase
+    private val signUpUseCase: SignUpUseCase,
+    private val nicknameValidationUseCase: NicknameValidationUseCase,
 ) : ViewModel() {
 
     private val _events = MutableSharedFlow<NicknameEvent>()
@@ -21,8 +28,18 @@ class NicknameViewModel @Inject constructor(
 
     val nickname = MutableStateFlow("")
 
+    val nicknameValidationState: StateFlow<NicknameValidationResult> =
+        nickname.map { nicknameValidationUseCase(it) }
+            .stateIn(
+                viewModelScope,
+                SharingStarted.WhileSubscribed(),
+                NicknameValidationResult.EMPTY
+            )
+
     fun signUp(googleToken: String) {
-        // TODO : 중복 검사 및 유효성 검사
+
+        if (nicknameValidationState.value != NicknameValidationResult.VALID) return
+
         signUpUseCase(googleToken = googleToken, nickname = nickname.value)
             .onEach {
                 _events.emit(NicknameEvent.NavigateToHome)
