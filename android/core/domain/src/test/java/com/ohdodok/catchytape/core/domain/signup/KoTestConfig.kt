7b0f032d0974d728e9e2fce5b5c1bf5f7ef8ab65
2taezeat <xorudfl772@naver.com>
2023-11-20 18:46:40 +0900

    chore: unit test-results path 변경 (디렉토리 추가)

--- android/core/domain/src/test/java/com/ohdodok/catchytape/core/domain/signup/KoTestConfig.kt
+++ android/core/domain/src/test/java/com/ohdodok/catchytape/core/domain/signup/KoTestConfig.kt
@@ -4,13 +4,13 @@ import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.extensions.Extension
 import io.kotest.extensions.junitxml.JunitXmlReporter
 
-class TestConfig : AbstractProjectConfig() {
+class KoTestConfig : AbstractProjectConfig() {
 
     override fun extensions(): List<Extension> = listOf(
         JunitXmlReporter(
             includeContainers = false, // don't write out status for all tests
             useTestPathAsName = true, // use the full test path (ie, includes parent test names)
-            outputDir = "../test-results/"
+            outputDir = "../test-results/excludeContainers"
         )
     )
 }
