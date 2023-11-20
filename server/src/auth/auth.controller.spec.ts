3b555eb7bb342a66884b4c868a1f1e78c2dc1c2b
Seokyung Lim <sk000801@naver.com>
2023-11-20 23:16:15 +0900

    chore : 테스트 코드 정상적으로 돌아가도록 수정
    Co-authored-by: khw3754 <khw3754@gmail.com >

--- server/src/auth/auth.controller.spec.ts
+++ server/src/auth/auth.controller.spec.ts
@@ -1,15 +1,34 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/entity/user.entity';
+import { Repository } from 'typeorm';
+import { JwtModule, JwtService } from '@nestjs/jwt';
 
 describe('AuthController', () => {
   let controller: AuthController;
+  let service: AuthService;
+  let jwtModule: JwtModule;
+  let repository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [JwtModule],
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    service = module.get<AuthService>(AuthService);
+    jwtModule = module.get<JwtModule>(JwtModule);
+    repository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
--- server/src/user/user.service.spec.ts
+++ server/src/user/user.service.spec.ts
@@ -1,15 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../entity/user.entity';
 
 describe('UserService', () => {
   let service: UserService;
+  let repository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    repository = module.get(getRepositoryToken(User))
   });
 
   it('should be defined', () => {
