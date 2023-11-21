3c0d1a51138081424661c22ea470b44e813ca7b0
hyungun <khw3754@gmail.com>
2023-11-21 15:56:38 +0900

    feat : /users/verify 구현
    * JWT Access Token 으로 사용자 검증
    * UserId 를 반환

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,14 +1,18 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   Post,
+  Req,
+  UseGuards,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { HTTP_STATUS_CODE } from 'src/httpStatusCode.enum';
 import { UserCreateDto } from 'src/dto/userCreate.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class AuthController {
@@ -30,5 +34,12 @@ export class AuthController {
     @Body() userCreateDto: UserCreateDto,
   ): Promise<{ accessToken: string }> {
     return this.authService.signup(userCreateDto);
+  }
+
+  @Get('verify')
+  @UseGuards(AuthGuard())
+  verifyToken(@Req() req): { userId: string } {
+    const user = req.user;
+    return { userId: user.user_id };
   }
 }
