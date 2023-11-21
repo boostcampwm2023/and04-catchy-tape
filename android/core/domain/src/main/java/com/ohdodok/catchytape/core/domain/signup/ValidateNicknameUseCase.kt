eda283af66ed21d230872038bef0cb418ec8e91d
algosketch <algosketch@gmail.com>
2023-11-21 14:35:23 +0900

    feat : flow에서 old value는 drop하기

--- android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/signup/ValidateNicknameUseCase.kt
+++ android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/signup/ValidateNicknameUseCase.kt
@@ -1,11 +1,13 @@
 package com.ohdodok.catchytape.core.domain.signup
 
 import com.ohdodok.catchytape.core.domain.repository.AuthRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.mapLatest
 import kotlinx.coroutines.flow.single
 import javax.inject.Inject
 
@@ -20,9 +22,9 @@ enum class NicknameValidationResult {
 class ValidateNicknameUseCase @Inject constructor(
     private val authRepository: AuthRepository,
 ) {
-    @OptIn(FlowPreview::class)
+    @OptIn(FlowPreview::class, ExperimentalCoroutinesApi::class)
     operator fun invoke(nicknameStream: Flow<String>): Flow<NicknameValidationResult> =
-        nicknameStream.map { nickname ->
+        nicknameStream.mapLatest { nickname ->
             val regex = "(^[ㄱ-ㅎ가-힣\\w_.]{2,10}$)".toRegex()
 
             val result = when {
