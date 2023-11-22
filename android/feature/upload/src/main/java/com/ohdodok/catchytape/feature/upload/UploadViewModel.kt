634e41e494384cc5539262da0d42843175cb7ff2
youlalala <reyoula@gmail.com>
2023-11-22 16:23:50 +0900

    feat : progress bar visibility 설정

--- android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadViewModel.kt
+++ android/feature/upload/src/main/java/com/ohdodok/catchytape/feature/upload/UploadViewModel.kt
@@ -6,19 +6,23 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import java.io.File
 import javax.inject.Inject
 import androidx.lifecycle.viewModelScope
+import com.ohdodok.catchytape.core.domain.usecase.GetFileUrlUseCase
 import com.ohdodok.catchytape.core.domain.usecase.GetMusicGenresUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
 @HiltViewModel
 class UploadViewModel @Inject constructor(
-    private val getMusicGenresUseCase: GetMusicGenresUseCase
+    private val getMusicGenresUseCase: GetMusicGenresUseCase,
+    private val getFileUrlUseCase: GetFileUrlUseCase
 ) : ViewModel() {
 
     val musicTitle = MutableStateFlow("")
@@ -41,6 +45,9 @@ class UploadViewModel @Inject constructor(
     private val _musicGenres: MutableStateFlow<List<String>> = MutableStateFlow(emptyList())
     val musicGenres = _musicGenres.asStateFlow()
 
+    private val _uploadUiState: MutableStateFlow<UploadUiState> = MutableStateFlow(UploadUiState())
+    val uploadUiState = _uploadUiState.asStateFlow()
+
     init {
         fetchGenres()
     }
@@ -54,11 +61,30 @@ class UploadViewModel @Inject constructor(
     fun uploadImage(imageUri: Uri) {
         // todo : image 파일을 업로드 한다.
         // todo : 반환 값을 uploadedImage에 저장한다.
+        imageUri.path?.let { path ->
+            getFileUrlUseCase(File(path)).onEach {
+                _thumbnailUrl.value = it
+            }.launchIn(viewModelScope)
+        }
     }
 
     fun uploadAudio(audioUri: Uri) {
         // todo : audio 파일을 업로드 한다.
-        val file = audioUri.path?.let { File(it) }
+        audioUri.path?.let { path ->
+            getFileUrlUseCase(File(path)).onEach {
+                _uploadUiState.value = uploadUiState.value.copy(isAudioLoading = false)
+                _audioUrl.value = it
+            }.onStart {
+                _uploadUiState.value = uploadUiState.value.copy(isAudioLoading = true)
+            }.catch {
+                _uploadUiState.value = uploadUiState.value.copy(isAudioLoading = false)
+            }.launchIn(viewModelScope)
+        }
     }
 
 }
+
+data class UploadUiState (
+    val isAudioLoading: Boolean = false,
+)
+
