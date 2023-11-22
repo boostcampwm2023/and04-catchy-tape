44ddc0b8caca33e985b724574c30984ae1b2047c
algosketch <algosketch@gmail.com>
2023-11-22 19:36:16 +0900

    feat : 앱 진입할 때, 네트워크 연결이 끊겼을 때 네트워크 상태 체크

--- android/app/src/main/java/com/ohdodok/catchytape/MainActivity.kt
+++ android/app/src/main/java/com/ohdodok/catchytape/MainActivity.kt
@@ -1,13 +1,50 @@
 package com.ohdodok.catchytape
 
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.NET_CAPABILITY_VALIDATED
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var connectivityManager: ConnectivityManager
+
+    private val networkCallback = object : ConnectivityManager.NetworkCallback() {
+        override fun onLost(network: Network) {
+            super.onLost(network)
+            checkNetworkState()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        connectivityManager = getSystemService(ConnectivityManager::class.java)
+        checkNetworkState()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        connectivityManager.registerDefaultNetworkCallback(networkCallback)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        connectivityManager.unregisterNetworkCallback(networkCallback)
+    }
+
+    private fun checkNetworkState() {
+        val activeNetwork = connectivityManager.activeNetwork
+        val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+        val isNetworkAvailable = capabilities?.hasCapability(NET_CAPABILITY_VALIDATED) ?: false
+
+        if(!isNetworkAvailable) {
+            Toast.makeText(this, "네트워크 연결을 확인해 주세요.", Toast.LENGTH_LONG).show()
+        }
     }
 }
