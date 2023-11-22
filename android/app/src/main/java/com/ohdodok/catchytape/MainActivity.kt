6a09914827931732e40e0b283b5509812fdeae4a
algosketch <algosketch@gmail.com>
2023-11-22 19:48:41 +0900

    refactor : 문자열 자원으로 추출

--- android/app/src/main/java/com/ohdodok/catchytape/MainActivity.kt
+++ android/app/src/main/java/com/ohdodok/catchytape/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.ohdodok.catchytape
 
 import android.net.ConnectivityManager
-import android.net.Network
-import android.net.NetworkCapabilities
 import android.net.NetworkCapabilities.NET_CAPABILITY_VALIDATED
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
+import com.ohdodok.catchytape.core.ui.R.string as uiString
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -29,8 +28,8 @@ class MainActivity : AppCompatActivity() {
         val capabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
         val isNetworkAvailable = capabilities?.hasCapability(NET_CAPABILITY_VALIDATED) ?: false
 
-        if(!isNetworkAvailable) {
-            Toast.makeText(this, "네트워크 연결을 확인해 주세요.", Toast.LENGTH_LONG).show()
+        if (!isNetworkAvailable) {
+            Toast.makeText(this, getString(uiString.check_network), Toast.LENGTH_LONG).show()
         }
     }
 }
