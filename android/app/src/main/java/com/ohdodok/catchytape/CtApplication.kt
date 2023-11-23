2f5cf6ed62f2677b61ef8220571ad3f046b6e32b
2taezeat <xorudfl772@naver.com>
2023-11-23 13:59:50 +0900

    feat : 불 필요한 timber override d 제거

--- android/app/src/main/java/com/ohdodok/catchytape/CtApplication.kt
+++ android/app/src/main/java/com/ohdodok/catchytape/CtApplication.kt
@@ -10,13 +10,6 @@ class CtApplication : Application() {
         super.onCreate()
         if (BuildConfig.DEBUG) {
             Timber.plant(object : Timber.DebugTree() {
-                override fun d(message: String?, vararg args: Any?) {
-                    if (message != null && message.isEmpty()) {
-                        super.d(getString(R.string.timber_blank_string), *args)
-                    } else {
-                        super.d(message, *args)
-                    }
-                }
 
                 override fun createStackElementTag(element: StackTraceElement): String {
                     return String.format(
