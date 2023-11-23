4d8cbdc5902ed4425af43a6bc8ed270660f3a5da
youlalala <reyoula@gmail.com>
2023-11-23 11:53:06 +0900

    feat : 음악 업로드 서버 통신 로직

--- android/core/data/src/main/java/com/ohdodok/catchytape/core/data/repository/MusicRepositoryImpl.kt
+++ android/core/data/src/main/java/com/ohdodok/catchytape/core/data/repository/MusicRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package com.ohdodok.catchytape.core.data.repository
 
 import com.ohdodok.catchytape.core.data.api.MusicApi
+import com.ohdodok.catchytape.core.data.model.MusicRequest
 import com.ohdodok.catchytape.core.domain.repository.MusicRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -15,6 +16,27 @@ class MusicRepositoryImpl @Inject constructor(
         when (response.code()) {
             // TODO : 네트워크 에러 로직 처리
             in 200..299 -> emit(response.body()?.genres ?: emptyList())
+            else -> throw RuntimeException("네트워크 에러")
+        }
+    }
+
+    override fun postMusic(
+        title: String,
+        imageUrl: String,
+        audioUrl: String,
+        genre: String
+    ): Flow<Unit> = flow {
+        val response = musicApi.postMusic(
+            MusicRequest(
+                title = title,
+                cover = imageUrl,
+                file = audioUrl,
+                genre = genre
+            )
+        )
+        when (response.code()) {
+            // TODO : 네트워크 에러 로직 처리
+            in 200..299 -> emit(response.body() ?: Unit)
             else -> throw RuntimeException("네트워크 에러")
         }
     }
