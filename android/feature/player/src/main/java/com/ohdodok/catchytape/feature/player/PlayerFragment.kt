7e1918a3837a0d4a743fff5f83c9d3d8314be1c8
algosketch <algosketch@gmail.com>
2023-11-24 01:06:31 +0900

    feat : 음악 플레이어 실행

--- android/feature/player/src/main/java/com/ohdodok/catchytape/feature/player/PlayerFragment.kt
+++ android/feature/player/src/main/java/com/ohdodok/catchytape/feature/player/PlayerFragment.kt
@@ -3,16 +3,37 @@ package com.ohdodok.catchytape.feature.player
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
+import androidx.media3.exoplayer.ExoPlayer
 import com.ohdodok.catchytape.core.ui.BaseFragment
 import com.ohdodok.catchytape.feature.player.databinding.FragmentPlayerBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PlayerFragment : BaseFragment<FragmentPlayerBinding>(R.layout.fragment_player) {
     private val viewModel: PlayerViewModel by viewModels()
+    private lateinit var player: Player
+    // todo : 실제 데이터로 변경
+    private val url = "https://kr.object.ncloudstorage.com/catchy-tape-bucket2/transform/transform/test/music.m3u8"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
 
         setupBackStack(binding.tbPlayer)
+
+        setupPlayer()
+    }
+
+    private fun setupPlayer() {
+        val mediaItem = MediaItem.fromUri(url)
+
+        player = ExoPlayer.Builder(requireContext()).build()
+        binding.playerView.player = player
+
+        player.setMediaItem(mediaItem)
+        player.prepare()
+        player.play()
     }
 }
