7584b1d2205d365298478d31157ef174d2ab2e4a
Seokyung Lim <sk000801@naver.com>
2023-11-23 17:32:59 +0900

    chore : 테스트 오류 수정

--- server/src/user/user.controller.spec.ts
+++ server/src/user/user.controller.spec.ts
@@ -4,19 +4,37 @@ import { UserService } from './user.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
 import { Repository } from 'typeorm';
+import { PlaylistService } from 'src/playlist/playlist.service';
+import { Music } from 'src/entity/music.entity';
+import { Music_Playlist } from 'src/entity/music_playlist.entity';
+import { Playlist } from 'src/entity/playlist.entity';
 
 describe('UserController', () => {
   let controller: UserController;
   let userService: UserService;
-  let repository;
+  let playlistService: PlaylistService;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
       providers: [
         UserService,
+        PlaylistService,
         {
           provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Playlist),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Music),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(Music_Playlist),
           useClass: Repository,
         },
       ],
@@ -24,7 +42,8 @@ describe('UserController', () => {
 
     controller = module.get<UserController>(UserController);
     userService = module.get<UserService>(UserService);
-    repository = module.get(getRepositoryToken(User));
+    playlistService = module.get<PlaylistService>(PlaylistService);
+    userRepository = module.get(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
