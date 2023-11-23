85e285778b15cc4748e09af58b84d8f589045589
2taezeat <xorudfl772@naver.com>
2023-11-24 00:16:17 +0900

    feat : CtErrorType 추가 및 분기 처리

--- android/core/ui/src/main/java/com/ohdodok/catchytape/core/ui/BaseFragment.kt
+++ android/core/ui/src/main/java/com/ohdodok/catchytape/core/ui/BaseFragment.kt
@@ -67,6 +67,16 @@ abstract class BaseFragment<VB : ViewDataBinding>(
             CtErrorType.CONNECTION -> R.string.error_message_connection
             CtErrorType.IO -> R.string.error_message_io
             CtErrorType.SSL_HAND_SHAKE -> R.string.error_message_ssl_hand_shake
+            CtErrorType.UN_KNOWN -> R.string.error_message_un_known
+            CtErrorType.NOT_EXIST_PLAYLIST_ON_USER -> R.string.error_message_not_exist_playlist_on_user
+            CtErrorType.NOT_EXIST_MUSIC -> R.string.error_message_not_exist_music
+            CtErrorType.ALREADY_ADDED -> R.string.error_message_already_added
+            CtErrorType.INVALID_INPUT_VALUE -> R.string.error_message_invalid_input_value
+            CtErrorType.NOT_EXIST_USER -> R.string.error_message_not_exist_user
+            CtErrorType.ALREADY_EXIST_EMAIL -> R.string.error_message_already_exist_email
+            CtErrorType.NOT_EXIST_GENRE -> R.string.error_message_not_exist_genre
+            CtErrorType.EXPIRED_TOKEN -> R.string.error_message_expired_token
+            CtErrorType.SERVICE -> R.string.error_message_service
         }
     }
 
