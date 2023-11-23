1cdc5d4019d6e46df0311fc23b85311a1ea47d0c
algosketch <algosketch@gmail.com>
2023-11-24 01:34:59 +0900

    feat : 음악 플레이어 재생, 일시정지

--- android/feature/player/src/main/java/com/ohdodok/catchytape/feature/player/PlayerFragment.kt
+++ android/feature/player/src/main/java/com/ohdodok/catchytape/feature/player/PlayerFragment.kt
@@ -14,8 +14,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class PlayerFragment : BaseFragment<FragmentPlayerBinding>(R.layout.fragment_player) {
     private val viewModel: PlayerViewModel by viewModels()
     private lateinit var player: Player
+
     // todo : 실제 데이터로 변경
-    private val url = "https://kr.object.ncloudstorage.com/catchy-tape-bucket2/transform/transform/test/music.m3u8"
+    private val url =
+        "https://kr.object.ncloudstorage.com/catchy-tape-bucket2/transform/transform/test/music.m3u8"
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,6 +26,7 @@ class PlayerFragment : BaseFragment<FragmentPlayerBinding>(R.layout.fragment_pla
         setupBackStack(binding.tbPlayer)
 
         setupPlayer()
+        collectEvents()
     }
 
     private fun setupPlayer() {
@@ -35,5 +38,16 @@ class PlayerFragment : BaseFragment<FragmentPlayerBinding>(R.layout.fragment_pla
         player.setMediaItem(mediaItem)
         player.prepare()
         player.play()
+    }
+
+    private fun collectEvents() {
+        repeatOnStarted {
+            viewModel.events.collect { event ->
+                when (event) {
+                    PlayerEvent.Play -> player.play()
+                    PlayerEvent.Pause -> player.pause()
+                }
+            }
+        }
     }
 }
--- android/feature/player/src/main/java/com/ohdodok/catchytape/feature/player/PlayerViewModel.kt
+++ android/feature/player/src/main/java/com/ohdodok/catchytape/feature/player/PlayerViewModel.kt
@@ -1,12 +1,55 @@
 package com.ohdodok.catchytape.feature.player
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+
+data class PlayerState(
+    val isPlaying: Boolean = true
+)
+
+sealed interface PlayerEvent {
+    data object Play : PlayerEvent
+    data object Pause : PlayerEvent
+}
 
 @HiltViewModel
 class PlayerViewModel @Inject constructor(
 
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(PlayerState())
+    val uiState: StateFlow<PlayerState> = _uiState.asStateFlow()
+
+    private val _events = MutableSharedFlow<PlayerEvent>()
+    val events: SharedFlow<PlayerEvent> = _events.asSharedFlow()
+
+    private fun sendEvent(event: PlayerEvent) {
+        viewModelScope.launch {
+            _events.emit(event)
+        }
+    }
+
+    fun play() {
+        sendEvent(PlayerEvent.Play)
+        _uiState.update {
+            it.copy(isPlaying = true)
+        }
+    }
+
+    fun pause() {
+        sendEvent(PlayerEvent.Pause)
+        _uiState.update {
+            it.copy(isPlaying = false)
+        }
+    }
 }
