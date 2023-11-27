a4d6ec1fa3ad2adff761dd3fb54ba5f7a68f7269
2taezeat <xorudfl772@naver.com>
2023-11-27 18:23:09 +0900

    feat : Home feature module, api 호출 예외 처리 적용

--- android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeViewModel.kt
+++ android/feature/home/src/main/java/com/ohdodok/catchytape/feature/home/HomeViewModel.kt
@@ -2,15 +2,22 @@ package com.ohdodok.catchytape.feature.home
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ohdodok.catchytape.core.domain.model.CtErrorType
+import com.ohdodok.catchytape.core.domain.model.CtException
 import com.ohdodok.catchytape.core.domain.model.Music
 import com.ohdodok.catchytape.core.domain.usecase.GetRecentUploadedMusic
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import javax.inject.Inject
 
 data class HomeUiState(
@@ -21,6 +28,21 @@ data class HomeUiState(
 class HomeViewModel @Inject constructor(
     private val getRecentUploadedMusicUseCase: GetRecentUploadedMusic
 ) : ViewModel() {
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        viewModelScope.launch {
+            if (throwable is CtException) {
+                _events.emit(HomeEvent.ShowMessage(throwable.ctError))
+            } else {
+                _events.emit(HomeEvent.ShowMessage(CtErrorType.UN_KNOWN))
+            }
+        }
+    }
+
+    private val viewModelScopeWithExceptionHandler = viewModelScope + exceptionHandler
+
+    private val _events = MutableSharedFlow<HomeEvent>()
+    val events = _events.asSharedFlow()
 
     private val _uiState = MutableStateFlow(HomeUiState())
     val uiState: StateFlow<HomeUiState> = _uiState.asStateFlow()
@@ -34,6 +56,10 @@ class HomeViewModel @Inject constructor(
                     )
                 }
             }
-            .launchIn(viewModelScope)
+            .launchIn(viewModelScopeWithExceptionHandler)
     }
+}
+
+sealed interface HomeEvent {
+    data class ShowMessage(val error: CtErrorType) : HomeEvent
 }
