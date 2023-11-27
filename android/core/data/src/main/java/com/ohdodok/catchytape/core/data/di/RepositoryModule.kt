2017c37ccf459487bdefc53572123eb85d3accc1
youlalala <reyoula@gmail.com>
2023-11-27 14:47:28 +0900

    refactor : user token repository 생성

--- android/core/data/src/main/java/com/ohdodok/catchytape/core/data/di/RepositoryModule.kt
+++ android/core/data/src/main/java/com/ohdodok/catchytape/core/data/di/RepositoryModule.kt
@@ -3,9 +3,11 @@ package com.ohdodok.catchytape.core.data.di
 import com.ohdodok.catchytape.core.data.repository.AuthRepositoryImpl
 import com.ohdodok.catchytape.core.data.repository.MusicRepositoryImpl
 import com.ohdodok.catchytape.core.data.repository.UrlRepositoryImpl
+import com.ohdodok.catchytape.core.data.repository.UserTokenRepositoryImpl
 import com.ohdodok.catchytape.core.domain.repository.AuthRepository
 import com.ohdodok.catchytape.core.domain.repository.MusicRepository
 import com.ohdodok.catchytape.core.domain.repository.UrlRepository
+import com.ohdodok.catchytape.core.domain.repository.UserTokenRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -22,9 +24,14 @@ interface RepositoryModule {
 
     @Binds
     @Singleton
+    fun bindUserTokenRepository(userTokenRepositoryImpl: UserTokenRepositoryImpl): UserTokenRepository
+
+    @Binds
+    @Singleton
     fun bindMusicRepository(musicRepositoryImpl: MusicRepositoryImpl): MusicRepository
 
     @Binds
     @Singleton
     fun bindUrlRepository(urlRepositoryImpl: UrlRepositoryImpl): UrlRepository
+
 }
---
+++ android/core/data/src/main/java/com/ohdodok/catchytape/core/data/repository/UserTokenRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.ohdodok.catchytape.core.data.repository
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.ohdodok.catchytape.core.domain.repository.UserTokenRepository
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class UserTokenRepositoryImpl @Inject constructor(
+    private val dataStore: DataStore<Preferences>
+) : UserTokenRepository {
+
+    private val accessTokenKey = stringPreferencesKey("accessToken")
+
+    override suspend fun getAccessToken(): String =
+        dataStore.data.map { preferences -> preferences[accessTokenKey] ?: "" }.first()
+
+    override suspend fun saveAccessToken(token: String) {
+        dataStore.edit { preferences -> preferences[accessTokenKey] = token }
+    }
+}
