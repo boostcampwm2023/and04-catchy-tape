2cac6754b41bd183e12ae231293adbdbbbcfbc62
algosketch <algosketch@gmail.com>
2023-11-28 23:20:07 +0900

    feat : 내가 업로드한 노래 데이터 바인딩

--- android/feature/mypage/src/main/java/com/ohdodok/catchytape/feature/mypage/MyPageViewModel.kt
+++ android/feature/mypage/src/main/java/com/ohdodok/catchytape/feature/mypage/MyPageViewModel.kt
@@ -1,17 +1,36 @@
 package com.ohdodok.catchytape.feature.mypage
 
 import androidx.lifecycle.ViewModel
+import com.ohdodok.catchytape.core.domain.model.Music
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
+data class MyPageUiState(
+    val myMusics: List<Music> = emptyList()
+)
 
 @HiltViewModel
 class MyPageViewModel @Inject constructor() : ViewModel() {
+
+    private val _uiState = MutableStateFlow(MyPageUiState())
+    val uiState: StateFlow<MyPageUiState> = _uiState.asStateFlow()
+
     private val _events = MutableSharedFlow<MyPageEvent>()
     val events: SharedFlow<MyPageEvent> = _events.asSharedFlow()
+
+    init {
+        fetchMyMusics()
+    }
+
+    private fun fetchMyMusics() {
+
+    }
 }
 
 
