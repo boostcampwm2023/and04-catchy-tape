bde83b26d87d8a566384c524902072d9a3e720ed
youlalala <reyoula@gmail.com>
2023-11-30 23:07:21 +0900

    test : music title validation 테스트 코드 작성

--- android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/usecase/upload/ValidateMusicTitleUseCase.kt
+++ android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/usecase/upload/ValidateMusicTitleUseCase.kt
@@ -1,12 +1,9 @@
 package com.ohdodok.catchytape.core.domain.usecase.upload
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.mapLatest
-
 class ValidateMusicTitleUseCase {
 
-    operator fun invoke(title: Flow<String>): Flow<Boolean> = title.mapLatest { title ->
+    operator fun invoke(title: String): Boolean {
         val regex = "(^[가-힣\\w]{1,}$)".toRegex()
-        regex.matches(title)
+        return regex.matches(title)
     }
 }
