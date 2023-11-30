9b0b89487793d1069b722221346a52f78881045e
youlalala <reyoula@gmail.com>
2023-11-30 22:09:33 +0900

    feat : 최근 재생 목록 가져오기

--- android/feature/player/src/main/java/com/ohdodok/catchytape/feature/player/PlayerViewModel.kt
+++ android/feature/player/src/main/java/com/ohdodok/catchytape/feature/player/PlayerViewModel.kt
@@ -2,30 +2,39 @@ package com.ohdodok.catchytape.feature.player
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ohdodok.catchytape.core.domain.model.CtErrorType
+import com.ohdodok.catchytape.core.domain.model.CtException
+import com.ohdodok.catchytape.core.domain.model.Music
+import com.ohdodok.catchytape.core.domain.repository.PlaylistRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import javax.inject.Inject
 
 data class PlayerState(
+    val playlist: List<Music> = emptyList(),
     val isPlaying: Boolean = false,
     val currentPositionSecond: Int = 0,
     val duration: Int = 0,
 )
 
 sealed interface PlayerEvent {
-    data class ShowError(val error: Exception) : PlayerEvent
+    data class ShowError(val error: CtErrorType) : PlayerEvent
 }
 
 @HiltViewModel
 class PlayerViewModel @Inject constructor(
-
+    private val playlistRepository: PlaylistRepository,
 ) : ViewModel(), PlayerEventListener {
 
     private val _uiState = MutableStateFlow(PlayerState())
@@ -33,6 +42,23 @@ class PlayerViewModel @Inject constructor(
 
     private val _events = MutableSharedFlow<PlayerEvent>()
     val events: SharedFlow<PlayerEvent> = _events.asSharedFlow()
+
+    private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
+        val errorType = if (throwable is CtException) throwable.ctError else CtErrorType.UN_KNOWN
+        viewModelScope.launch { _events.emit(PlayerEvent.ShowError(errorType)) }
+    }
+
+    private val viewModelScopeWithExceptionHandler = viewModelScope + exceptionHandler
+
+    init {
+        getRecentPlaylist()
+    }
+
+    private fun getRecentPlaylist() {
+        playlistRepository.getRecentPlaylist().onEach { recentPlaylist ->
+            _uiState.update { it.copy(playlist = recentPlaylist) }
+        }.launchIn(viewModelScopeWithExceptionHandler)
+    }
 
     private fun sendEvent(event: PlayerEvent) {
         viewModelScope.launch {
