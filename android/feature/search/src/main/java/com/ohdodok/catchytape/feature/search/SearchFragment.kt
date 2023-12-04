06b4d219d525bfbd4be39383d83af2c0749e0035
algosketch <algosketch@gmail.com>
2023-12-05 01:52:00 +0900

    feat : 검색어 입력

--- android/feature/search/src/main/java/com/ohdodok/catchytape/feature/search/SearchFragment.kt
+++ android/feature/search/src/main/java/com/ohdodok/catchytape/feature/search/SearchFragment.kt
@@ -1,9 +1,7 @@
 package com.ohdodok.catchytape.feature.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.ohdodok.catchytape.core.ui.BaseFragment
 import com.ohdodok.catchytape.feature.search.databinding.FragmentSearchBinding
@@ -11,9 +9,6 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
-    init {
-        println("아니 왜 안 돼!!")
-    }
 
     private val viewModel: SearchViewModel by viewModels()
 
--- android/feature/search/src/main/java/com/ohdodok/catchytape/feature/search/SearchViewModel.kt
+++ android/feature/search/src/main/java/com/ohdodok/catchytape/feature/search/SearchViewModel.kt
@@ -2,11 +2,25 @@ package com.ohdodok.catchytape.feature.search
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
+
+data class SearchUiState(
+    val keyword: String = "",
+)
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
 
 ) : ViewModel() {
 
+    private val _uiState = MutableStateFlow(SearchUiState())
+    val uiState: StateFlow<SearchUiState> = _uiState.asStateFlow()
+
+    fun updateKeyword(newKeyword: String) {
+        _uiState.update { it.copy(keyword = newKeyword) }
+    }
 }
