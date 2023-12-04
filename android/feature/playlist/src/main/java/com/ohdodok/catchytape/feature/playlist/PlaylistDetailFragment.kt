f2e91bbbd4017573bc9b49f72dfb22b1a44e05bc
youlalala <reyoula@gmail.com>
2023-12-04 22:19:53 +0900

    feat : 플레이리스트 상세 화면 UI

--- android/feature/playlist/src/main/java/com/ohdodok/catchytape/feature/playlist/PlaylistDetailFragment.kt
+++ android/feature/playlist/src/main/java/com/ohdodok/catchytape/feature/playlist/PlaylistDetailFragment.kt
@@ -1,11 +1,17 @@
 package com.ohdodok.catchytape.feature.playlist
 
+import android.os.Bundle
+import android.view.View
 import com.ohdodok.catchytape.core.ui.BaseFragment
 import com.ohdodok.catchytape.feature.playlist.databinding.FragmentPlaylistDetailBinding
 
 class PlaylistDetailFragment :
     BaseFragment<FragmentPlaylistDetailBinding>(R.layout.fragment_playlist_detail) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupBackStack(binding.tbPlaylistDetail)
+    }
 
 }
 
