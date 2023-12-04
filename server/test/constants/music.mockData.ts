dc69999ec3dfe3f1f8cc3436dccc7826e9f01824
Seokyung Lim <sk000801@naver.com>
2023-12-05 02:11:35 +0900

    chore : 테스트를 위한 user, Music mock 데이터 추가

--- server/test/constants/music.mockData.ts
+++ server/test/constants/music.mockData.ts
@@ -3,6 +3,23 @@ import { Music } from 'src/entity/music.entity';
 import { Genres } from 'src/constants';
 import { User } from 'src/entity/user.entity';
 
+export const user: User = {
+  user_id: '2d4b574c-b370-4e2e-ac4f-e1eca8eea090',
+  nickname: 'master2',
+  photo: null,
+  user_email: 'sugamypapa@gmail.com',
+  created_at: new Date('2023-12-04 23:57:50.693000'),
+} as User;
+
+export const realMusicCreateInfo: MusicCreateDto = {
+  music_id: '6bcd415d-4a22-43f0-9daa-64199442ff2d',
+  title: 'mysongone',
+  cover:
+    'https://kr.object.ncloudstorage.com/catchy-tape-bucket2/image/cover/6bcd415d-4a22-43f0-9daa-64199442ff2d/image.png',
+  file: 'https://kr.object.ncloudstorage.com/catchy-tape-bucket2/music/6bcd415d-4a22-43f0-9daa-64199442ff2d/music.mp3',
+  genre: 'acoustic' as Genres,
+};
+
 export const musicCreateInfo: MusicCreateDto = {
   music_id: 'music_id',
   title: '내가부른노래예시1',
