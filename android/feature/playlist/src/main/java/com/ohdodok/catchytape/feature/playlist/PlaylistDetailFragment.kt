d2eb3ddf6193e658c6b9fa383292e12a4640fe7e
youlalala <reyoula@gmail.com>
2023-12-04 18:56:51 +0900

    feat : PlaylistsFragment -> PlaylistDetailFragment 화면 전환 이벤트 처리

--- android/feature/playlist/src/main/java/com/ohdodok/catchytape/feature/playlist/PlaylistDetailFragment.kt
+++ android/feature/playlist/src/main/java/com/ohdodok/catchytape/feature/playlist/PlaylistDetailFragment.kt
@@ -3,7 +3,9 @@ package com.ohdodok.catchytape.feature.playlist
 import com.ohdodok.catchytape.core.ui.BaseFragment
 import com.ohdodok.catchytape.feature.playlist.databinding.FragmentPlaylistDetailBinding
 
-class PlaylistDetailFragment : BaseFragment<FragmentPlaylistDetailBinding>(R.layout.fragment_playlist_detail) {
+class PlaylistDetailFragment :
+    BaseFragment<FragmentPlaylistDetailBinding>(R.layout.fragment_playlist_detail) {
 
 
 }
+
