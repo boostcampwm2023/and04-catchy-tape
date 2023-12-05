f3f3c1d9f736f01d4ff5987ed0e33fd91f9f461d
2taezeat <xorudfl772@naver.com>
2023-12-05 18:19:43 +0900

    feat : Fragment에 setupRecyclerView 추가

--- android/feature/search/src/main/java/com/ohdodok/catchytape/feature/search/SearchFragment.kt
+++ android/feature/search/src/main/java/com/ohdodok/catchytape/feature/search/SearchFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.viewModels
 import com.ohdodok.catchytape.core.ui.BaseFragment
+import com.ohdodok.catchytape.core.ui.MusicAdapter
+import com.ohdodok.catchytape.core.ui.Orientation
 import com.ohdodok.catchytape.core.ui.toMessageId
 import com.ohdodok.catchytape.feature.search.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +20,11 @@ class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_sea
         binding.viewModel = viewModel
 
         observeEvents()
+        setupRecyclerView()
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvSearch.adapter = MusicAdapter(Orientation.VERTICAL)
     }
 
     private fun observeEvents() {
