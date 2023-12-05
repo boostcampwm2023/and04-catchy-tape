54128330e63a0fbece46ae231bc18b7ed7775cac
Seokyung Lim <sk000801@naver.com>
2023-12-05 21:42:28 +0900

    chore : Music 모듈에 GreenEyeService 모듈 의존성 추가

--- server/src/music/music.module.ts
+++ server/src/music/music.module.ts
@@ -6,10 +6,16 @@ import { Music } from 'src/entity/music.entity';
 import { AuthModule } from 'src/auth/auth.module';
 import { UploadService } from 'src/upload/upload.service';
 import { NcloudConfigService } from 'src/config/ncloud.config';
+import { GreenEyeService } from 'src/config/greenEye.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Music]), AuthModule],
   controllers: [MusicController],
-  providers: [MusicService, UploadService, NcloudConfigService],
+  providers: [
+    MusicService,
+    UploadService,
+    NcloudConfigService,
+    GreenEyeService,
+  ],
 })
 export class MusicModule {}
