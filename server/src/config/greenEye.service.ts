9c5da22dfd2691958b693505a53fa1528ba2edd4
Seokyung Lim <sk000801@naver.com>
2023-12-05 21:46:36 +0900

    feat : 이미지 업로드 시 유해 이미지 탐지를 위한 Green Eye Service 추
    * fetch를 통해 요청을 보내 이미지 탐지 결과를 받아옴

---
+++ server/src/config/greenEye.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { CatchyException } from 'src/config/catchyException';
+import { ERROR_CODE } from 'src/config/errorCode.enum';
+import { GreenEyeResponseDto } from 'src/dto/greenEye.response.dto';
+import { HTTP_STATUS_CODE } from 'src/httpStatusCode.enum';
+
+@Injectable()
+export class GreenEyeService {
+  greenEyeSecretKey: string;
+  greenEyeRequestUrl: string;
+
+  contentType: string = 'application/json';
+
+  constructor(private readonly configService: ConfigService) {
+    this.greenEyeSecretKey = configService.get<string>('GREEN_EYE_SECRET_KEY');
+    this.greenEyeRequestUrl = configService.get<string>(
+      'GREEN_EYE_REQUEST_URL',
+    );
+  }
+
+  private getTimeStamp(): number {
+    return new Date().getTime();
+  }
+
+  private getRequestInit(
+    imageUrl: string,
+  ): Record<string, string | Record<string, string>> {
+    return {
+      method: 'POST',
+      headers: {
+        'X-GREEN-EYE-SECRET': this.greenEyeSecretKey,
+        'Content-Type': this.contentType,
+      },
+      body: JSON.stringify({
+        version: 'V1',
+        requestId: 'requestId',
+        timestamp: this.getTimeStamp(),
+        images: [
+          {
+            name: 'image',
+            url: imageUrl,
+          },
+        ],
+      }),
+    };
+  }
+
+  async getResultOfNormalImage(imageUrl: string): Promise<GreenEyeResponseDto> {
+    try {
+      return await fetch(
+        this.greenEyeRequestUrl,
+        this.getRequestInit(imageUrl),
+      ).then((res) => res.json());
+    } catch {
+      throw new CatchyException(
+        'INVALID_GREEN_EYE_REQUEST',
+        HTTP_STATUS_CODE.BAD_REQUEST,
+        ERROR_CODE.INVALID_GREEN_EYE_REQUEST,
+      );
+    }
+  }
+}
