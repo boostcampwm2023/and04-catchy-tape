2543e0b16b60a57d1e25e0c63d3e1aa0a0cbfce9
algosketch <algosketch@gmail.com>
2023-12-06 14:15:19 +0900

    refactor : 필요없는 코드 제거

--- android/core/ui/src/main/java/com/ohdodok/catchytape/core/ui/SizeUtil.kt
+++
@@ -1,8 +0,0 @@
-package com.ohdodok.catchytape.core.ui
-
-import android.content.Context
-
-fun Context.fromDpToPx(dp: Float): Float {
-    val density = resources.displayMetrics.density
-    return dp * density
-}
