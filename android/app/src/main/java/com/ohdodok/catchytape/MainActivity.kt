0ebd68bb2b7ae106ee31538af204bd227df802fc
2taezeat <xorudfl772@naver.com>
2023-12-07 14:17:45 +0900

    feat : 불필요한 코드 제거 및 databinding 적용

--- android/app/src/main/java/com/ohdodok/catchytape/MainActivity.kt
+++ android/app/src/main/java/com/ohdodok/catchytape/MainActivity.kt
@@ -24,8 +24,6 @@ import androidx.navigation.ui.setupWithNavController
 import com.ohdodok.catchytape.databinding.ActivityMainBinding
 import com.ohdodok.catchytape.feature.player.PlayerListener
 import com.ohdodok.catchytape.feature.player.PlayerViewModel
-import com.ohdodok.catchytape.feature.player.changeMoveBtnState
-import com.ohdodok.catchytape.feature.player.changePlayBtnState
 import com.ohdodok.catchytape.feature.player.getMediasWithMetaData
 import com.ohdodok.catchytape.feature.player.millisecondsPerSecond
 import com.ohdodok.catchytape.feature.player.moveNextMedia
@@ -144,11 +142,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun showPlayerController() {
         binding.pcvController.visibility = View.VISIBLE
-        player.changePlayBtnState(binding.pcvController.playImageView)
-        player.changeMoveBtnState(
-            binding.pcvController.moveNextImageView,
-            binding.pcvController.movePreviousImageView
-        )
     }
 
     private fun setupPlayer() {
@@ -191,26 +184,17 @@ class MainActivity : AppCompatActivity() {
             if (playViewModel.uiState.value.isPlaying) player.pause()
             else player.play()
         }
-        player.changePlayBtnState(binding.pcvController.playImageView)
     }
 
     private fun setupPreviousButton() {
         binding.pcvController.setOnPreviousButtonClick {
             player.movePreviousMedia()
-            player.changeMoveBtnState(
-                binding.pcvController.moveNextImageView,
-                binding.pcvController.movePreviousImageView
-            )
         }
     }
 
     private fun setupNextButton() {
         binding.pcvController.setOnNextButtonClick {
             player.moveNextMedia()
-            player.changeMoveBtnState(
-                binding.pcvController.moveNextImageView,
-                binding.pcvController.movePreviousImageView
-            )
         }
     }
 }
