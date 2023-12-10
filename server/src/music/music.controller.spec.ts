6e02f7c57e5c69b33658388d841785e662ea78c3
Seokyung Lim <sk000801@naver.com>
2023-12-10 13:36:16 +0900

    feat : Music e2e 테스트 추가
    * overrideProvider를 통해 모킹한 jwt 클래스 주입

--- server/src/music/music.controller.spec.ts
+++ server/src/music/music.controller.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import { ExecutionContext } from '@nestjs/common/interfaces';
+import * as request from 'supertest';
 import { MusicService } from './music.service';
 import { UploadService } from 'src/upload/upload.service';
 import { NcloudConfigService } from 'src/config/ncloud.config';
@@ -8,25 +8,29 @@ import { ConfigService } from '@nestjs/config';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Music } from 'src/entity/music.entity';
-import { user } from 'test/constants/music.mockData';
+import { musicCreateInfo, user } from 'test/constants/music.mockData';
 import { MusicController } from './music.controller';
 import { User } from 'src/entity/user.entity';
 import { AuthService } from 'src/auth/auth.service';
 import { JwtService } from '@nestjs/jwt';
-import { AuthGuard } from '@nestjs/passport';
+import { PassportModule } from '@nestjs/passport';
 import { GreenEyeService } from 'src/config/greenEye.service';
+import { JwtStrategy } from 'src/auth/jwt.strategy';
 
 describe('UploadController', () => {
   let app: INestApplication;
   let musicController: MusicController;
+  let musicService: MusicService;
   let uploadService: UploadService;
   let cloudService: NcloudConfigService;
   let configService: ConfigService;
   let authService: AuthService;
+  let musicRepository: Repository<Music>;
+  let mockJwtStrategy = { validate: () => user };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [],
+      imports: [PassportModule],
       controllers: [MusicController],
       providers: [
         MusicService,
@@ -36,6 +40,7 @@ describe('UploadController', () => {
         GreenEyeService,
         AuthService,
         JwtService,
+        JwtStrategy,
         {
           provide: getRepositoryToken(Music),
           useClass: Repository,
@@ -46,14 +51,8 @@ describe('UploadController', () => {
         },
       ],
     })
-      .overrideGuard(AuthGuard)
-      .useValue({
-        canActivate: (context: ExecutionContext) => {
-          const req = context.switchToHttp().getRequest();
-          req.user = user;
-          return true;
-        },
-      })
+      .overrideProvider(JwtStrategy)
+      .useValue(mockJwtStrategy)
       .compile();
 
     configService = module.get<ConfigService>(ConfigService);
@@ -61,6 +60,8 @@ describe('UploadController', () => {
     uploadService = module.get<UploadService>(UploadService);
     authService = module.get<AuthService>(AuthService);
     musicController = module.get<MusicController>(MusicController);
+    musicService = module.get<MusicService>(MusicService);
+    musicRepository = module.get(getRepositoryToken(Music));
 
     app = module.createNestApplication();
     await app.init();
@@ -69,6 +70,18 @@ describe('UploadController', () => {
   describe('MusicController 환경 확인', () => {
     it('MusicController should be defined', () => {
       expect(musicController).toBeDefined();
+    });
+  });
+
+  describe('Music 요청 관련 테스트', () => {
+    it('User가 요청 보낸 음악 정보를 DB에 저장한다.', async () => {
+      request(app.getHttpServer)
+        .post('/musics')
+        .send({ musicCreateInfo })
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.music_id).toEqual(musicCreateInfo.music_id);
+        });
     });
   });
 });
