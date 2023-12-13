93fae0e2a0c81d68d484c2f9a544a71acab1c2bf
2taezeat <xorudfl772@naver.com>
2023-12-13 12:31:59 +0900

    feat : throttleFist 구현

---
+++ android/core/domain/src/main/java/com/ohdodok/catchytape/core/domain/utils/CoroutineUtils.kt
@@ -0,0 +1,21 @@
+package com.ohdodok.catchytape.core.domain.utils
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+fun <T> Flow<T>.throttleFist(windowDuration: Long): Flow<T> = flow {
+    var windowStartTime = System.currentTimeMillis()
+    var emitted = false
+    collect { value ->
+        val currentTime = System.currentTimeMillis()
+        val delta = currentTime - windowStartTime
+        if (delta >= windowDuration) {
+            windowStartTime += delta / windowDuration * windowDuration
+            emitted = false
+        }
+        if (!emitted) {
+            emit(value)
+            emitted = true
+        }
+    }
+}
