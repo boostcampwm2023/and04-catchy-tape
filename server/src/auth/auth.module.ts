e84eccf3cb86af26c37b1bc0de767099d41f9b3b
seokyung <sk000801@naver.com>
2024-01-26 01:01:37 +0900

    feat : auth 모듈에 redis 관련 설정 추가
    * Config로부터 redis 관련 설정 값을 받아 넣어줌
    * cache 저장소를 redis로 설정

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule, CacheStore } from '@nestjs/cache-manager';
 import { JwtStrategy } from './jwt.strategy';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { Logger } from 'winston';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
   imports: [
@@ -16,8 +18,28 @@ import { Logger } from 'winston';
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET_KEY'),
-        signOptions: { expiresIn: Number.MAX_SAFE_INTEGER },
+        signOptions: { expiresIn: '60s' },
       }),
+      inject: [ConfigService],
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        const store = await redisStore({
+          socket: {
+            host: configService.get<string>('REDIS_HOST_IP'),
+            port: configService.get<number>('REDIS_PORT'),
+          },
+          password: configService.get<string>('REDIS_PASSWORD'),
+          legacyMode: true,
+        });
+
+        return {
+          store: store as unknown as CacheStore,
+          ttl: configService.get<number>('REDIS_TTL'),
+        };
+      },
       inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([User]),
