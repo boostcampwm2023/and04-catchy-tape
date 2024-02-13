d338a42559346c8195084f1698a67f5f2d897071
seokyung <sk000801@naver.com>
2024-02-13 22:24:44 +0900

    feat : spring 컨테이너 dockerfile 작성, 확인용 API 추가
    * spring 서버 도커 이미지를 빌드하기 위한 dockerfile 작성
    * 이후 서버에 정상적으로 띄워졌는지 확인하기 위한 API 작성

---
+++ server/spring/src/main/java/catchytape/spring/HelloCatchyTape.java
@@ -0,0 +1,12 @@
+package catchytape.spring;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HelloCatchyTape {
+    @GetMapping("/test")
+    public String test() {
+        return "hello this is catchy-tape!";
+    }
+}
