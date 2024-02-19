29df90282f6a709d14ab83c2dcf07d0235529b93
seokyung <sk000801@naver.com>
2024-02-19 15:43:15 +0900

    fix : 테스트 코드 변경

--- server/nest/src/app.controller.spec.ts
+++ server/nest/src/app.controller.spec.ts
@@ -16,7 +16,7 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Catchy Tape!');
+      expect(appController.getHello()).toBe('Hello Catchy Tape');
     });
   });
 });
